1babce4f785198dde30784ac54c371056f15b44a
yarosvet93 <yarosvet93@mail.ru>
2023-03-20 23:15:19 +0300

    chat_id

--- closepoll.php
+++ closepoll.php
@@ -14,16 +14,17 @@ $date_t = date('Y-m-d');
 $query = $db->query("SELECT id_poll_message FROM tb_trainings WHERE date_t = '$date_t'");
 foreach ($query AS $training) {
   $data = [
-    'chat_id' => $chat_id_my,
+    'chat_id' => $chat_id,
     'message_id' => $training['id_poll_message']
   ];	
 $url1 =  $url . "/stopPoll?" . http_build_query($data);
-$response = Message::send($url1);
-$db->exec("INSERT INTO tb_json (update_id, update_text) VALUES ('closepoll','$response')"); 
-$update = json_decode($response, TRUE);
-$total_voter_count = $update['result']['total_voter_count'];
-$id = $update['result']['id'];
-$db->exec("UPDATE tb_trainings SET total_voter_count  = '$total_voter_count'  WHERE id_poll = '$id' "); 
+print_r ($url1);
+// $response = Message::send($url1);
+// $db->exec("INSERT INTO tb_json (update_id, update_text) VALUES ('closepoll','$response')"); 
+// $update = json_decode($response, TRUE);
+// $total_voter_count = $update['result']['total_voter_count'];
+// $id = $update['result']['id'];
+// $db->exec("UPDATE tb_trainings SET total_voter_count  = '$total_voter_count'  WHERE id_poll = '$id' "); 
 
 }
 //file_get_contents($url."/stopPoll?chat_id=" . $chat_id . "&message_id=");
