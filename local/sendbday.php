148cd2cd018bd3317cb7fabed5c17c5b668f65dd
yarosvet93 <yarosvet93@mail.ru>
2024-04-06 20:27:39 +0300

    sendbday

--- local/sendbday.php
+++ local/sendbday.php
@@ -3,13 +3,22 @@ require_once '../config.php';
 require_once '../src/Messages.php';
 use Source\Action\Message;
 header('Content-type: text/plain; charset=utf-8');
-//$chat_id_my = '287781777';
-$chat_id = '111895196';
-$message = 'Тестирую';
-$data = [
-	'chat_id' => $chat_id,
-    'text' => $message
-];
-$url2 = $url . "/sendMessage?" . http_build_query($data);
-$response = Message::send($url2);
-$db->exec("INSERT INTO tb_json (update_id, update_text) VALUES ('sendtext','$response')"); 
+//$chat_id_my = '287781777 Овчинников Владимир';
+$morrow= strtotime("+1 day");
+$date = date("m-d",$morrow);
+$chat_id = '287781777';
+$message ='';
+$check_bday = $db->query("SELECT * FROM tb_players WHERE DATE_FORMAT(bday, '%m-%d') = '$date'");
+if (!empty($check_bday)){
+    foreach ($check_bday AS $d){
+        $message = $message . $d['fio'] . "\n";
+    }
+    $message = 'ЗАВТРА день рождения у:' . "\n" . $message;
+    $data = [
+        'chat_id' => $chat_id,
+        'text' => $message
+    ];
+    $url2 = $url . "/sendMessage?" . http_build_query($data);
+    $response = Message::send($url2);
+    $db->exec("INSERT INTO tb_json (update_id, update_text) VALUES ('sendtext','$response')"); 
+    }
