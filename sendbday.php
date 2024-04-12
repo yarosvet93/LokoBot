3429e546bf0eef3937d97ce3477b109a28695ebf
yarosvet93 <yarosvet93@mail.ru>
2024-04-12 13:29:55 +0300

    sendbday

--- sendbday.php
+++ sendbday.php
@@ -1,10 +1,10 @@
 <?php
 require_once 'processing.php';
 header('Content-type: text/plain; charset=utf-8');
-//$chat_id_my = '287781777 Овчинников Владимир';
+//$chat_id_my = '267638282 Овчинников Владимир';
 $morrow= strtotime("+1 day");
 $date = date("m-d",$morrow);
-$chat_id = '287781777';
+$chat_id = '267638282';
 $message ='';
 $check_bday = $db->query("SELECT * FROM tb_players WHERE DATE_FORMAT(bday, '%m-%d') = '$date'");
 if (!empty($check_bday)){ 
@@ -12,5 +12,5 @@ if (!empty($check_bday)){
     $message = "ЗАВТРА день рождения у:\n" . $message;
     $data = http_build_query(['chat_id' => $chat_id, 'text' => $message]);
     $response = backslash_to_mysql(file_get_contents($url . "/sendMessage?" . $data));
-    $db->exec("INSERT INTO tb_json (update_id, update_text) VALUES ('sendtext','$response')"); 
+    $db->exec("INSERT INTO tb_json (update_id, update_text) VALUES ('sendbday','$response')"); 
     }
